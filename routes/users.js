f26104e9499df00f5b01b59b5ff6ea2d96ea4033
Nickolas Nikolic <subclass@gmail.com>
2015-10-21 16:57:07 -0500

    wiring up

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [{firstName:'bill',lastName:'brother',email:'a@b.c'}];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
